2b56017e2fc7c141dfeb31fc1e42df276672c3bb
boko91 <boko91@gmail.com>
2018-03-23 09:55:49 -0700

    Uploaded batch-relink-eps-to-ai-CUSTOM
    Customization based off this original example: https://forums.adobe.com/thread/1696010

---
+++ batch-relink-eps-to-ai-CUSTOM.js
@@ -0,0 +1,20 @@
+var doc = app.activeDocument;
+var links = doc.allGraphics;
+for (var i=links.length-1; i>=0; i--) {
+  if (links[i].itemLink.name.match(/.+\.eps$/)) {
+      var oldname = links[i].itemLink.filePath;
+      // alert(oldname);
+      // break;
+      var newname = oldname.replace(".eps", ".ai");
+      // alert(newfile);
+      // break;
+      var newfile = File(newname);
+      // alert(newfile);
+      // break;
+      if (newfile.exists) {
+        // alert(newfile);
+        // break;
+        links[i].itemLink.relink(newfile);
+      }
+  }
+}
